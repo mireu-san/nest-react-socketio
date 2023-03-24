153bf69085f87debd152ac76c8f0cb713764ffee
Jongwan Kim <starmireu@gmail.com>
2023-03-24 15:08:13 +0900

    updated UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Chat from './Chat';
+import styled from 'styled-components';
 
 const App = () => {
   return (
-    <div>
-      <h1>Test purpose - nestjs and react</h1>
-      <Chat />
-    </div>
+    <AppStyle>
+      <div>
+        <h1>Test purpose - nestjs and react</h1>
+        <Chat />
+      </div>
+    </AppStyle>
   );
 };
 
 export default App;
+
+const AppStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin-top: 10%;
+`
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import styled from 'styled-components';
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
@@ -16,8 +17,8 @@ const Chat = () => {
 
     // Unsubscribe from "message" event when component unmounts
     return () => {
-      // socket.off('message', handleMessage);
-      socket.disconnect('message', handleMessage);
+      socket.off('message', handleMessage);
+      // socket.disconnect('message', handleMessage);
     };
   }, []);
 
@@ -25,23 +26,102 @@ const Chat = () => {
     setMessages((messages) => [...messages, message]);
   };
 
-  const sendMessage = () => {
+  // const sendMessage = () => {
+  //   socket.emit('message', input);
+  //   setInput('');
+  // };
+  const sendMessage = (event) => {
+    event.preventDefault();
     socket.emit('message', input);
     setInput('');
   };
 
   return (
-    <div>
-      <div>
+    <ChatStyle>
+      <ChatHeader>
+        <div>Telegram Chat</div>
+        <div>Online</div>
+      </ChatHeader>
+      <ChatBody>
         {messages.map((message, index) => (
-          <div key={index}>{message}</div>
+          <ChatMessage key={index}>{message}</ChatMessage>
         ))}
-      </div>
-      <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button onClick={sendMessage}>Send</button>
-    </div>
+      </ChatBody>
+      <ChatInputArea onSubmit={sendMessage}>
+        <ChatInput
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <ChatButton>Send</ChatButton>
+      </ChatInputArea>
+    </ChatStyle>
   );
 };
 
 export default Chat;
 
+const ChatStyle = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: #f5f5f5;
+  font-family: sans-serif;
+`;
+
+const ChatHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  background-color: #0088cc;
+  color: white;
+  font-size: 20px;
+`;
+
+const ChatBody = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  padding: 15px 20px;
+`;
+
+const ChatInputArea = styled.form`
+  display: flex;
+  align-items: center;
+  background-color: white;
+  padding: 15px 20px;
+  border-top: 1px solid #ddd;
+`;
+
+const ChatInput = styled.input`
+  flex: 1;
+  border: none;
+  outline: none;
+  font-size: 16px;
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const ChatButton = styled.button`
+  border: none;
+  outline: none;
+  background-color: #0088cc;
+  color: white;
+  font-size: 16px;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin-left: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0077b3;
+  }
+`;
+
+const ChatMessage = styled.div`
+  background-color: #fff;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  padding: 10px;
+  box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
+`;
